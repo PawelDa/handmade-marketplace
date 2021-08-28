f6c5ed73d035ae8506b8f5dce8d00abc272d074d
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-28 19:10:28 +0100

    HomePage component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-meny'>
+      <div className='item-menu'>
+        <div className='content'>
+          <h1 className='title'>Candles</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='item-menu'>
+        <div className='content'>
+          <h1 className='title'>Bookmarks</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='item-menu'>
+        <div className='content'>
+          <h1 className='title'>Clothes</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;

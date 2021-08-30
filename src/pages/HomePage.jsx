06e0deb607c3a57ec7f1f3c4dbafe3ce05d78644
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-30 19:49:27 +0100

    changed stylesheets organization

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Directory from '../components/Directory';
 
-import '../assets/stylesheets/components/HomePage.scss';
+import '../assets/stylesheets/pages/HomePage.scss';
 
 const HomePage = () => (
   <div className='homepage'>

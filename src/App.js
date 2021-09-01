eef5f9840f26cf154ebc2bf0e28edd7a8a974743
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-01 10:35:10 +0100

    sign in page component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
+import SignInAndSignUpPage from './pages/SignInAndSignUpPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
       <div id='icon-link'>Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
---
+++ src/pages/SignInAndSignUpPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../assets/stylesheets/pages/SignInAndSignUpPage.scss';
+
+const SignInAndSignUpPage = () => (
+  <div className='sign-in-and-sing-up'>SIGN IN</div>
+);
+
+export default SignInAndSignUpPage;

6d126d8fa14b87333552a9695c8d04071d317031
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-04 12:29:07 +0100

    debugged add to chart function

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from '../assets/images/shopping-bag.sv
 
 import '../assets/stylesheets/components/CartIcon.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
@@ -18,4 +18,14 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (acucumulatedQuantity, cartItem) => acucumulatedQuantity + cartItem.quantity,
+    0
+  )
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartIcon);

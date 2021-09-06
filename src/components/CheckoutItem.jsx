4d005fb072fa5c175c12de158b725c8e49703fef
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-06 11:48:36 +0100

    added + and - to change quantity

--- src/components/CheckoutItem.jsx
+++ src/components/CheckoutItem.jsx
@@ -17,13 +17,13 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       </span>
       <span className='quantity'>
         <div className='arrow' onClick={() => removeItem(cartItem)}>
-          &#10094;
+          -&nbsp;&nbsp;
         </div>
         <span className='value'>
           {quantity}
         </span>
         <div className='arrow' onClick={() => addItem(cartItem)}>
-          &#10095;
+          &nbsp;&nbsp;+
         </div>
       </span>
       <span className='price'>

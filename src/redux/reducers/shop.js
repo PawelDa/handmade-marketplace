28904e2e8e45ca03c73faf036315d9bf94c7c4e0
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-08 18:41:40 +0100

    delete promise midlleware

--- src/redux/reducers/shop.js
+++ src/redux/reducers/shop.js
@@ -4,7 +4,7 @@ const initialState = {
   collections: SHOP_DATA
 };
 
-const shopReducer = (state =initialState, action) => {
+const shopReducer = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;

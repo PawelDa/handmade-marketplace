675b7fcb9443f7722aad14ef6391981b51eed3c6
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-22 21:14:04 +0100

    added title icon to shop page

--- src/components/CollectionPreview.jsx
+++ src/components/CollectionPreview.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import CollectionItem from './CollectionItem';
+import { ReactComponent as Logo } from '../assets/images/candles.svg';
 
 import '../assets/stylesheets/components/CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='title-container'>
+      <Logo className='title-icon' />
+      <titleIcon className='title-icon' />
+      <img className='title-icon' src='/../assets/images/candles.png' alt=''/>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+    </div>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)

45b25a7435b495e0dcf30f4224dec0c5f1c90be0
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-23 22:19:59 +0100

    transformed header into styled component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,36 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/config';
-
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from '../../redux/selectors/user';
 import { selectCartHidden } from '../../redux/selectors/cart';
 
-import { ReactComponent as Logo } from '../../assets/images/default-monochrome.svg';
 import CartIcon from '../cart/CartIcon';
 import CartDropdown from '../cart/CartDropdown';
 
+import {
+  MarketLogo,
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionDiv,
+  OptionLink
+} from '../../assets/stylesheets/components/layout/Header';
+
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+  <HeaderContainer>
+    <LogoContainer to='/'>
+      <MarketLogo />
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>
         SHOP
-      </Link>
-      {
-        currentUser ?
-        <div className='option' onClick={() => auth.signOut()}><i className="fas fa-sign-out-alt fa-sm"></i>&nbsp;SIGN OUT</div>
+      </OptionLink>
+      {currentUser ?
+        <OptionDiv onClick={() => auth.signOut()}>
+          <i className="fas fa-sign-out-alt fa-sm"></i>&nbsp;SIGN OUT
+        </OptionDiv>
         :
-        <Link className='option' to='/signin'>SIGN IN</Link>
+        <OptionLink to='/signin'>
+          SIGN IN
+        </OptionLink>
       }
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({

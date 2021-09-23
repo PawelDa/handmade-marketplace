b623556feae14e17006186a576fad5557ee1634b
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-23 05:44:44 +0100

    added title icons to shop categories

--- src/components/CollectionPreview.jsx
+++ src/components/CollectionPreview.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
 import CollectionItem from './CollectionItem';
-import { ReactComponent as Logo } from '../assets/images/candles.svg';
 
 import '../assets/stylesheets/components/CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <div className='title-container'>
-      <Logo className='title-icon' />
-      <titleIcon className='title-icon' />
-      <img className='title-icon' src='/../assets/images/candles.png' alt=''/>
+      <img className='title-icon' src={`/shop-icons/${title.toLowerCase()}.svg`} alt=''/>
       <h1 className='title'>{title.toUpperCase()}</h1>
     </div>
     <div className='preview'>

81c4e4dbd360e1d758635aa106a8ac9c537d7791
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-23 05:51:24 +0100

    added link to shop cattegories

--- src/components/CollectionPreview.jsx
+++ src/components/CollectionPreview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import CollectionItem from './CollectionItem';
 
@@ -6,10 +7,12 @@ import '../assets/stylesheets/components/CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <div className='title-container'>
-      <img className='title-icon' src={`/shop-icons/${title.toLowerCase()}.svg`} alt=''/>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-    </div>
+    <Link to={`/shop/${title.toLowerCase()}`}>
+      <div className='title-container'>
+        <img className='title-icon' src={`/shop-icons/${title.toLowerCase()}.svg`} alt=''/>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+      </div>
+    </Link>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
